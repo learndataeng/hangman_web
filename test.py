829aa3ba90a0ec07950c3b218d223bec747a2794
learndataeng <135051097+learndataeng@users.noreply.github.com>
2023-06-10 19:40:13 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+"""Unit test cases for hangman game."""
+import unittest
+import app as hangman
+
+class HangmanTestCase(unittest.TestCase):
+
+    # def setUp(self):
+    #
+
+    # checkCorrectAnswer(correctLetters, secretWord)
+    def test_checkCorrectAnswer(self):
+        answer = hangman.checkCorrectAnswer("baon", "baboon")
+        self.assertTrue(answer)
+
+    def test_checkWrongAnswer(self):
+        answer = hangman.checkWrongAnswer("zebrio", "zebra")
+        self.assertTrue(answer)
+		
+    def test_1(self):
+        answer = hangman.checkCorrectAnswer("bazn", "baboon")
+        self.assertFalse(answer)	
+   
+    def test_2(self):
+        answer = hangman.checkCorrectAnswer("", " ")
+        self.assertFalse(answer)		
+    
+    def test_3(self):
+        answer = hangman.checkCorrectAnswer("ZEBRA", "zebra")
+        self.assertFalse(answer)		
+
+		
+if __name__ == "__main__":
+    unittest.main()
